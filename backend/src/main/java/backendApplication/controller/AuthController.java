4a08263f8c3401372f78a5fe3edd37eee7242705
nelsontss <nelsontss@gmail.com>
2020-05-26 22:20:57 +0100

    Auth controller initial commit, functional tests in jmeter for authcontroller

---
+++ backend/src/main/java/backendApplication/controller/AuthController.java
@@ -0,0 +1,57 @@
+package backendApplication.controller;
+
+import backendApplication.dao.UserService;
+import backendApplication.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.NoSuchElementException;
+
+@RestController
+public class AuthController {
+
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value = "/sing_up", method = RequestMethod.POST)
+    public ResponseEntity registeUser(@RequestBody User user) {
+
+        try {
+            userService.get(user.getUsername());
+
+            return new ResponseEntity(HttpStatus.CONFLICT);
+        } catch ( NoSuchElementException e) {
+            try {
+                userService.save(user);
+            }catch (Exception ex) {
+                // logger ex.printStackTrace();
+                return new ResponseEntity(HttpStatus.NOT_ACCEPTABLE);
+            }
+
+            return new ResponseEntity(HttpStatus.OK);
+        }catch ( InvalidDataAccessApiUsageException e) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+
+    @RequestMapping(value = "/sing_in", method = RequestMethod.POST)
+    public ResponseEntity login(@RequestBody User user) {
+
+        try {
+            User u = userService.get(user.getUsername());
+            if(u.getPassword().equals(user.getPassword())){
+                return new ResponseEntity(HttpStatus.OK);
+            }
+
+            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        }catch ( NoSuchElementException e) {
+            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        }catch ( InvalidDataAccessApiUsageException e) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+    }
+}
