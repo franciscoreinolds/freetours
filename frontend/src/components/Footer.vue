526a03d093e213ca89a0f6e8488e2b081a0d379b
joaop21 <jpsilva9898@gmail.com>
2020-06-30 00:40:04 +0100

    Add same color

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -2,41 +2,35 @@
     <div>
         <v-footer
         class = "footer"
-        color = "black"
+        dense
+        dark
         height = 150px
         max-height = 150px
         >
-            <v-card
-            class = "my_card"
-            height = "120px"
-            min-width = "200px"
-            color = "black"
+            <v-card-text
+                    class = "text-center white--text"
             >
-                <v-card-text
-                class = "my_card_text"
-                >
-                    <v-btn
-                    v-for="icon in icons"
-                    :key = "icon.index"
-                    class = "mx-4"
-                    dark
-                    icon
-                    >
-                        <v-icon
+                <v-btn
+                        v-for="icon in icons"
+                        :key = "icon.index"
+                        class = "mx-4"
                         dark
-                        size = "24px"
-                        >
-                            {{icon}}
-                        </v-icon>
-                    </v-btn>
-                </v-card-text>
-                <v-divider></v-divider>
-                <v-card-text
-                class = "text-center white--text"
+                        icon
                 >
-                    {{ new Date().getFullYear() }} - <strong>FreeTours</strong>
-                </v-card-text>
-            </v-card>
+                    <v-icon
+                            dark
+                            size = "24px"
+                    >
+                        {{icon}}
+                    </v-icon>
+                </v-btn>
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-text
+                    class = "text-center white--text"
+            >
+                {{ new Date().getFullYear() }} - <strong>FreeTours</strong>
+            </v-card-text>
         </v-footer>
     </div>
 </template>
