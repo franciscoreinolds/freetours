47f8e12cb432a089da7ccd6188f2f97c010b008e
joaop21 <jpsilva9898@gmail.com>
2020-06-29 23:30:36 +0100

    Reset Password

--- backend/src/main/java/backendApplication/controller/AuthController.java
+++ backend/src/main/java/backendApplication/controller/AuthController.java
@@ -1,9 +1,11 @@
 package backendApplication.controller;
 
 import backendApplication.MyUserDetailsService;
+import backendApplication.model.dao.PasswordResetTokenService;
 import backendApplication.utils.JwtUtil;
 import backendApplication.model.dao.UserService;
 import backendApplication.model.entities.User;
+import backendApplication.viewmodel.PasswordChange;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.http.HttpStatus;
@@ -15,7 +17,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.NoSuchElementException;
+import java.util.UUID;
 
 @RestController
 public class AuthController {
@@ -25,6 +29,9 @@ public class AuthController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private PasswordResetTokenService passwordResetTokenService;
 
     @Autowired
     private AuthenticationManager authenticationManager;
@@ -75,4 +82,62 @@ public class AuthController {
 
         return ResponseEntity.ok(jwt);
     }
+
+
+    @RequestMapping(value = "/reset_password", method = RequestMethod.POST)
+    public ResponseEntity<String> resetPassword(@RequestBody User userBody) {
+
+        if(userBody.getEmail() == null)
+            return new ResponseEntity<>("An email must be sent as a parameter", HttpStatus.BAD_REQUEST);
+
+        User user = userService.findUserByEmail(userBody.getEmail());
+
+        if (user == null)
+            return new ResponseEntity<>("A valid email must be sent as a parameter", HttpStatus.BAD_REQUEST);
+
+        String token = UUID.randomUUID().toString();
+        passwordResetTokenService.createPasswordResetTokenForUser(user, token);
+
+        // send mail to user
+        // ..
+        // ..
+
+        return new ResponseEntity<>("A reset password mail was sent to your email", HttpStatus.CREATED);
+
+    }
+
+
+    @RequestMapping(value = "/change_password", method = RequestMethod.POST)
+    public ResponseEntity<String> changePassword(@RequestParam("token") String token, @RequestBody PasswordChange passwordChange) {
+
+        String result = passwordResetTokenService.validatePasswordResetToken(token);
+
+        if(result == null){
+
+            User user = passwordResetTokenService.getUserByPasswordResetToken(token);
+
+            if(passwordChange.getPassword().equals(passwordChange.getPassword_confirmation())) {
+
+                user.setPassword(passwordEncoder.encode(passwordChange.getPassword()));
+                userService.save(user);
+
+                passwordResetTokenService.deleteAllByUser(token);
+
+                return new ResponseEntity<>("Password changed successfully", HttpStatus.OK);
+
+            } else {
+
+                return new ResponseEntity<>("Password and Password confirmation are different", HttpStatus.BAD_REQUEST);
+
+            }
+
+        }
+        else if (result.equals("Expired"))
+            return new ResponseEntity<>("Token expired. Please request a new one", HttpStatus.GONE);
+
+        else
+            return new ResponseEntity<>("Token does not exist", HttpStatus.NOT_FOUND);
+
+    }
+
 }
