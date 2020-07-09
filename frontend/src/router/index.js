e7f6f51335940d5f6f423d5521d1fc97e3accd56
joaop21 <jpsilva9898@gmail.com>
2020-07-09 12:18:09 +0100

    Remove unused imports

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../components/Home.vue'
-import Login from '../components/Login.vue'
 import SignUp from '../components/SignUp.vue'
 import Profile from '../components/Profile.vue'
 import Search from '../components/Search.vue'
