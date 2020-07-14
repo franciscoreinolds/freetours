0410d10f85e6631c3c5afb19f1724cc88eafe01e
Diogo Braga <diogofiliperfbraga@gmail.com>
2020-07-14 02:42:43 +0100

    unsubscribeScheduling & more

--- backend/src/main/java/backendApplication/model/SwapManager.java
+++ backend/src/main/java/backendApplication/model/SwapManager.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.concurrent.CompletableFuture;
 
@@ -20,7 +22,13 @@ public class SwapManager {
 
     @Async
     public CompletableFuture addSchedule(Scheduling scheduling) throws InterruptedException{
-        long finishesIn = scheduling.getDate().getTime() - new Date().getTime();
+        //long finishesIn = scheduling.getDate().getTime() - new Date().getTime();
+
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime schedulingMoment = scheduling.getDate();
+        Duration duration = Duration.between(now, schedulingMoment);
+        long finishesIn = Math.abs(duration.toMinutes());
+
         System.out.println(finishesIn);
         Thread.sleep(finishesIn);
         Tour t = scheduling.getTour();
